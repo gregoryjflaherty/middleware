b7dd42e17dc41fe94cd43c3a19f7c6d3391fc035
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-08-24 09:39:28 -0400

    Create user auth middleware

--- server.js
+++ server.js
@@ -7,13 +7,22 @@ app.get('/', (req, res) => {
     res.send('Home Page')
 })
 
-app.get('/users', (req, res) => {
+app.get('/users', authorizeUsersAcess, (req, res) => {
     res.send('Users Page')
 })
 
-app.listen(3000, () => console.log('Server Started'))
 
 function loggingMiddleware(req, res, next) {
     console.log(`${new Date().toISOString()}: ${req.originalUrl}`)
     next()
 }
+
+function authorizeUsersAcess(req, res, next) {
+    if (req.query.admin === 'true') {
+        next()
+    } else {
+        res.send('ERROR: You must be an admin')
+    }
+}
+
+app.listen(3000, () => console.log('Server Started'))
